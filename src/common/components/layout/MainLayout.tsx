023bc8f048055b69ac252cc09fea4e1056fc64cd
TobiWeiss <tobias.m.weiss@gmx.de>
2025-10-07 11:16:33 +0200

    Adjusted background pattern

--- src/common/components/layout/MainLayout.tsx
+++ src/common/components/layout/MainLayout.tsx
@@ -11,9 +11,21 @@ export const MainLayout = () => {
   }
 
   return (
-    <div className="h-screen flex flex-col overflow-hidden bg-custom-primary-light dark:bg-custom-primary-dark">
+    <div className="min-h-screen w-full bg-[#fafafa] relative text-gray-900">
+      {/* Diagonal Grid with Light */}
+      <div
+        className="absolute inset-0 z-0 pointer-events-none"
+        style={{
+          backgroundImage: `
+            repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px),
+          repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0.1) 0, rgba(0, 0, 0, 0.1) 1px, transparent 1px, transparent 20px)
+          `,
+          backgroundSize: "40px 40px",
+        }}
+      />
+      
       {/* Fixed header */}
-      <header className="flex-none bg-custom-primary-light dark:bg-custom-primary-dark rounded-b-3xl shadow-md">
+      <header className="flex-none bg-custom-primary-light dark:bg-custom-primary-dark rounded-b-3xl shadow-md relative z-10">
         <div className="max-w-8xl px-20 mx-auto flex justify-between items-center py-3">
           <Logo onClick={goToInitPage} />
           <div className="flex items-center gap-4">
@@ -25,17 +37,6 @@ export const MainLayout = () => {
 
       {/* Scrollable content area */}
       <div className="flex-1 relative overflow-hidden">
-        {/* Background Image */}
-        <div 
-          className="absolute inset-0 z-0 opacity-100 dark:opacity-100"
-          style={{
-            backgroundImage: 'url(/src/common/assets/images/bg-light.svg)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-          }}
-        />
-
         {/* Scrollable main content */}
         <main className="h-full overflow-auto relative z-10">
           <div className="max-w-8xl mx-auto p-4">
